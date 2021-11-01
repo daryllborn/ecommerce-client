468c9257a002b2653c4ad2a58e38f21123260b09
Daryll Born <born.dd@gmail.com>
2021-11-01 23:39:11 +0100

    Changed product layout into grid

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,10 +5,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Container = styled.div`
-  padding: 20px;
-  display: flex;
+  justify-self: center;
+  padding: 30px;
+  //margin-top: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  //grid-gap: 20px;
+  /*display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: space-between;*/
 `;
 
 const Products = ({ filter, sort, category }) => {
